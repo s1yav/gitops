97e27e6a8d82ada3208538eb485289196e26405f
s1yav <88557944+s1yav@users.noreply.github.com>
2026-07-14 23:19:03 -0400

    feat: update entrypoint in index.ts
    Update the index.ts Pulumi entrypoint to export the new trigger and repository resources.

--- index.ts
+++ index.ts
@@ -1,4 +1,7 @@
 // Pulumi Entrypoint
 // Imports and executes all sub-stacks / infrastructure layers
 
-// import "./triggers";
+// import "./s1yav-components/s1yav-artifact-registry";
+// import "./s1yav-components/s1yav-git-event-interceptor";
+// import "./s1yav-components/s1yav-triggers";
+// import "./s1yav-components/s1yav-git-bridge";
