61dc36ecb5f72db72c47be727fea335fa8a8a09d
s1yav <88557944+s1yav@users.noreply.github.com>
2026-07-18 08:11:38 -0400

    chore: add cloudbuild service account as a member of owner

--- s1yav-github/cloudbuild-serviceaccount.ts
+++ s1yav-github/cloudbuild-serviceaccount.ts
@@ -13,37 +13,9 @@ export const s1yavCloudbuildServiceAccount = new Account(`${stackName}-cloudbuil
     description: "User-managed service account for executing Cloud Build triggers",
 });
 
-// reader iammember
-export const s1yavCloudbuildServiceAccountReader = new gcp.secretmanager.SecretIamMember(`${stackName}-cloudbuild-sa-reader`, {
-    secretId: pulumiConfig.requireSecret("tokenId"),
-    role: "roles/reader",
-    member: s1yavCloudbuildServiceAccount.account.email.apply(email => `serviceAccount:${email}`),
-});
-
-// Writer iammember
-export const s1yavCloudbuildServiceAccountWriter = new gcp.secretmanager.SecretIamMember(`${stackName}-cloudbuild-sa-writer`, {
-    secretId: pulumiConfig.requireSecret("tokenId"),
-    role: "roles/writer",
-    member: s1yavCloudbuildServiceAccount.account.email.apply(email => `serviceAccount:${email}`),
-});
-
-// DevOps
-export const cloudbuildSaDevOps = new gcp.projects.IAMMember(`${stackName}-cloudbuild-devOps`, {
+// owner iammember
+export const s1yavCloudbuildServiceAccountOwner = new gcp.projects.IAMMember(`${stackName}-cloudbuild-sa-owner`, {
     project: projectId,
-    role: "roles/iam.devOps",
-    member: s1yavCloudbuildServiceAccount.account.email.apply(email => `serviceAccount:${email}`),
-});
-
-// Grant Secret Accessor permission to the Cloud Build service account for the Pulumi token
-export const pulumiTokenAccessor = new gcp.secretmanager.SecretIamMember(`${stackName}-pulumi-token-accessor`, {
-    secretId: pulumiConfig.requireSecret("tokenId"),
-    role: "roles/secretmanager.secretAccessor",
-    member: s1yavCloudbuildServiceAccount.account.email.apply(email => `serviceAccount:${email}`),
-});
-
-// Grant Secret Accessor permission to the Cloud Build service account for the GitHub token
-export const githubTokenAccessor = new gcp.secretmanager.SecretIamMember(`${stackName}-github-token-accessor`, {
-    secretId: githubConfig.requireSecret("tokenId"),
-    role: "roles/secretmanager.secretAccessor",
+    role: "roles/owner",
     member: s1yavCloudbuildServiceAccount.account.email.apply(email => `serviceAccount:${email}`),
 });
