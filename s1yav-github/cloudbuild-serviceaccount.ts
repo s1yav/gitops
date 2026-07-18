f4afd70c1ee9f28eaec97ec0b0f2dd1da335ef73
s1yav <88557944+s1yav@users.noreply.github.com>
2026-07-18 08:01:55 -0400

    chore: add cloudbuild reader and writer

--- s1yav-github/cloudbuild-serviceaccount.ts
+++ s1yav-github/cloudbuild-serviceaccount.ts
@@ -13,6 +13,27 @@ export const s1yavCloudbuildServiceAccount = new Account(`${stackName}-cloudbuil
     description: "User-managed service account for executing Cloud Build triggers",
 });
 
+// reader iammember
+export const s1yavCloudbuildServiceAccountReader = new gcp.secretmanager.SecretIamMember(`${stackName}-cloudbuild-sa-reader`, {
+    secretId: pulumiConfig.requireSecret("tokenId"),
+    role: "roles/reader",
+    member: s1yavCloudbuildServiceAccount.account.email.apply(email => `serviceAccount:${email}`),
+});
+
+// Writer iammember
+export const s1yavCloudbuildServiceAccountWriter = new gcp.secretmanager.SecretIamMember(`${stackName}-cloudbuild-sa-writer`, {
+    secretId: pulumiConfig.requireSecret("tokenId"),
+    role: "roles/writer",
+    member: s1yavCloudbuildServiceAccount.account.email.apply(email => `serviceAccount:${email}`),
+});
+
+// DevOps
+export const cloudbuildSaDevOps = new gcp.projects.IAMMember(`${stackName}-cloudbuild-devOps`, {
+    project: projectId,
+    role: "roles/iam.devOps",
+    member: s1yavCloudbuildServiceAccount.account.email.apply(email => `serviceAccount:${email}`),
+});
+
 // Grant Secret Accessor permission to the Cloud Build service account for the Pulumi token
 export const pulumiTokenAccessor = new gcp.secretmanager.SecretIamMember(`${stackName}-pulumi-token-accessor`, {
     secretId: pulumiConfig.requireSecret("tokenId"),
@@ -24,12 +45,5 @@ export const pulumiTokenAccessor = new gcp.secretmanager.SecretIamMember(`${stac
 export const githubTokenAccessor = new gcp.secretmanager.SecretIamMember(`${stackName}-github-token-accessor`, {
     secretId: githubConfig.requireSecret("tokenId"),
     role: "roles/secretmanager.secretAccessor",
-    member: s1yavCloudbuildServiceAccount.account.email.apply(email => `serviceAccount:${email}`),
-});
-
-// DevOps
-export const cloudbuildSaDevOps = new gcp.projects.IAMMember(`${stackName}-cloudbuild-devOps`, {
-    project: projectId,
-    role: "roles/iam.devOps",
     member: s1yavCloudbuildServiceAccount.account.email.apply(email => `serviceAccount:${email}`),
 });
