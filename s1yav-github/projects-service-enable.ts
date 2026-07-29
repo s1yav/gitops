acddef8082edba7eba8f3a6ede4f246f68e18381
s1yav <88557944+s1yav@users.noreply.github.com>
2026-07-29 17:03:34 -0400

    Refactor projects-service-enable.ts to add Artifact Registry log writer and reader IAM members
    - Extracted Artifact Registry service account to a reusable variable.
    - Added gcp.projects.IAMMember for roles/logging.logWriter.
    - Added gcp.projects.IAMMember for roles/artifactregistry.reader.
    
    ### Pulumi Refresh Output (pulumi refresh --preview-only)
    
    Previewing refresh (s1yav-github)
    View Live: https://app.pulumi.com/sriyav0599-gmail-com/gitops/s1yav-github/previews/430cce33-095d-45a7-8869-700db3c2d045
    
    Resources:
        47 unchanged
    
    ### Pulumi Preview Output (pulumi preview)
    
    Previewing update (s1yav-github)
    View Live: https://app.pulumi.com/sriyav0599-gmail-com/gitops/s1yav-github/previews/69e11d53-d7f1-4e01-8380-01a9cc30793c
    
     +  gcp:projects:IAMMember s1yav-github-artifactregistry-log-writer create
     +  gcp:projects:IAMMember s1yav-github-artifactregistry-reader create
    
    Resources:
        + 2 to create
        47 unchanged

--- s1yav-github/projects-service-enable.ts
+++ s1yav-github/projects-service-enable.ts
@@ -1,7 +1,9 @@
+import * as gcp from "@pulumi/gcp";
 import { Service } from "../constructs/projects/service";
 import { gcpConfig, stackName } from "./configuration";
 
 const projectId = gcpConfig.require("project");
+const artifactRegistryServiceAccount = `serviceAccount:service-${projectId}@gcp-sa-artifactregistry.iam.gserviceaccount.com`;
 
 // Enable Secret Manager API
 export const secretManagerService = new Service(`${stackName}-secretmanager-api`, {
@@ -26,6 +28,20 @@ export const artifactRegistryService = new Service(`${stackName}-artifactregistr
     projectId: projectId,
     serviceName: "artifactregistry.googleapis.com",
 });
+
+// Grant Logging Log Writer role to Artifact Registry service account using gcp.projects.IAMMember
+export const artifactRegistryLogWriter = new gcp.projects.IAMMember(`${stackName}-artifactregistry-log-writer`, {
+    project: projectId,
+    role: "roles/logging.logWriter",
+    member: artifactRegistryServiceAccount,
+}, { dependsOn: [artifactRegistryService.service] });
+
+// Grant Artifact Registry Reader role using gcp.projects.IAMMember
+export const artifactRegistryReader = new gcp.projects.IAMMember(`${stackName}-artifactregistry-reader`, {
+    project: projectId,
+    role: "roles/artifactregistry.reader",
+    member: artifactRegistryServiceAccount,
+}, { dependsOn: [artifactRegistryService.service] });
 
 // Enable IAM API
 export const iamService = new Service(`${stackName}-iam-api`, {
